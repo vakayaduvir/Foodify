2b567f833fe2921c32fdab8ddc150eb7b305678e
vakayaduvir <vakanani17@gmail.com>
2025-09-21 17:34:05 +0530

    update

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ app.use('/vendor',vendorRoutes);
 app.use('/firm',firmRoutes);
 app.use('/product',productRoutes);
 app.use('/uploads',express.static('uploads'));
+
+app.use('/',(req,res)=>{
+    res.send('<h1>welcome to the page<h1>');
+});
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
-app.use('/',(req,res)=>{
-    res.send('<h1>welcome to the page<h1>');
-})
