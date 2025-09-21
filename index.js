8334efeef6e2a618571560eedaf4e60abf156dce
vakayaduvir <vakanani17@gmail.com>
2025-09-21 16:48:45 +0530

    updated

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
 app.use('/',(res,req)=>{
-    res.send(<h1>welcome to the page</h1>);
+    res.send(`<h1>welcome to the page<h1>`);
 })
